864b809bb4899710cb2678823e5a5e08912446c9
youyoung00 <you889@hanmail.net>
2021-12-06 12:02:36 +0900

    하단 공지만 스크롤 구현Expended

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -26,168 +26,188 @@ class HomeScreen extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+         ..._buildMenu(),// 리스트 안에 리스트를 넣을 경우 : 스프레드 연산자 ...
+          _buildAds(controller),
+          _buildNotice()
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildMenu (){
+    return [
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
             children: [
-              Column(
-                children: [
-                  Image.network(
-                    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.network(
-                    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.network(
-                    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.network(
-                    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Column(
-                children: [
-                  Image.network(
-                    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.network(
-                    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
-                  Image.network(
-                    "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
-                    width: 80,
-                    height: 80,
-                    fit: BoxFit.cover,
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Text(
-                    '택시',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                ],
+              Image.network(
+                "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
               ),
               SizedBox(
-                height: 80,
+                height: 10,
+              ),
+              Text(
+                '택시',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Image.network(
+                "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
                 width: 80,
-              )
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '택시',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Image.network(
+                "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '택시',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Image.network(
+                "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '택시',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+        ],
+      ),
+      SizedBox(
+        height: 30,
+      ),
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              Image.network(
+                "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '택시',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Image.network(
+                "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '택시',
+                style: TextStyle(fontSize: 20),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              Image.network(
+                "https://pds.joongang.co.kr/news/component/htmlphoto_mmdata/201702/18/htm_20170218114544126552.jpg",
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Text(
+                '택시',
+                style: TextStyle(fontSize: 20),
+              ),
             ],
           ),
           SizedBox(
-            height: 150,
-            child: PageView(
-              scrollDirection: Axis.horizontal,
-              controller: controller,
-              children: fakeAds.map((e) => ADView(ad: e,)).toList(),
-              // children: <Widget>[
-              //   ADView(
-              //     ad: fakeAds[0]
-              //     // title: '일어나 회사가야지',
-              //     // color: Colors.red,
-              //   ),
-              //   ADView(
-              //     ad: fakeAds[1],
-              //     // title: '제목',
-              //     // color: Colors.blue,
-              //   ),
-              //   ADView(
-              //     ad: fakeAds[2],
-              //     // title: '제목',
-              //     // color: Colors.yellow,
-              //   ),
-              // ],
-            ),
+            height: 80,
+            width: 80,
           )
         ],
+      ),
+    ];
+  }
+
+  Widget _buildAds(PageController controller) {
+    return SizedBox(
+          height: 150,
+          child: PageView(
+            scrollDirection: Axis.horizontal,
+            controller: controller,
+            children: fakeAds.map((e) => ADView(ad: e,)).toList(),
+            // children: <Widget>[
+            //   ADView(
+            //     ad: fakeAds[0]
+            //     // title: '일어나 회사가야지',
+            //     // color: Colors.red,
+            //   ),
+            //   ADView(
+            //     ad: fakeAds[1],
+            //     // title: '제목',
+            //     // color: Colors.blue,
+            //   ),
+            //   ADView(
+            //     ad: fakeAds[2],
+            //     // title: '제목',
+            //     // color: Colors.yellow,
+            //   ),
+            // ],
+          ),
+        );
+  }
+
+  Widget _buildNotice(){
+    return Expanded(
+      child: ListView(
+        shrinkWrap: true,
+        children: List.generate(50, (index) => Text('공지 ${index}')),
       ),
     );
   }
